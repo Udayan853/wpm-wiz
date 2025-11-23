f62b931c98b5da3d235ae25cedd32221171df2a3
Udayan <udayank23@gmail.com>
2025-11-23 14:30:15 +0530

    ehc: Update next config file for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? '/wpm-wiz' : '',
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
